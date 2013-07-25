9d91995ce4a75ea23eabe6f1903fa70c1659be62
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-25 23:35:00 +0100

    remove count part

--- www/js/employeelist.js
+++ www/js/employeelist.js
@@ -16,7 +16,7 @@ function getEmployeeList() {
 					'<img src="pics/' + employee.picture + '"/>' +
 					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
 					'<p>' + employee.title + '</p>' +
-					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+					'</a></li>');
 		});
 		$('#employeeList').listview('refresh');
 	});
