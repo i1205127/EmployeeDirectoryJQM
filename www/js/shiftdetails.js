585d013ef639384145f386153dd82ab6b327dec6
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 00:57:18 +0100

    adsfaf

--- www/js/shiftdetails.js
+++ www/js/shiftdetails.js
@@ -1,6 +1,6 @@
 $('#detailsPage').live('pageshow', function(event) {
 	var id = getUrlVars()["id"];
-	$.getJSON(serviceURL + 'geshifts.php?id='+id, displayEmployee);
+	$.getJSON(serviceURL + 'getshifts.php?id='+id, displayEmployee);
 });
 
 function displayEmployee(data) {
@@ -17,53 +17,25 @@ function displayEmployee(data) {
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
 				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
 		
-		$('#actionList').append('<li><a href="tel:' + managerPhone + '"><h3>Call Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 
-		$('#actionList').append('<li><a href="mailto:' + managerEmail + '"><h3>Email Bank Manager</h3>' +
-				'<p>' + managerEmail + '</p></a></li>');
-
-		$('#actionList').append('<li><a href="sms:' + managerPhone + '"><h3>SMS Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 
 	} else if (employee.wardid == 2){
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
 				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
 	
-		$('#actionList').append('<li><a href="tel:' + managerPhone + '"><h3>Call Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
-
-		$('#actionList').append('<li><a href="mailto:' + managerEmail + '"><h3>Email Bank Manager</h3>' +
-				'<p>' + managerEmail + '</p></a></li>');
-
-		$('#actionList').append('<li><a href="sms:' + managerPhone + '"><h3>SMS Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');				
+				
 	
 	} else if (employee.wardid == 3){
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
 				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
 
-		$('#actionList').append('<li><a href="tel:' + managerPhone + '"><h3>Call Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 
-		$('#actionList').append('<li><a href="mailto:' + managerEmail + '"><h3>Email Bank Manager</h3>' +
-				'<p>' + managerEmail + '</p></a></li>');
-
-		$('#actionList').append('<li><a href="sms:' + managerPhone + '"><h3>SMS Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 				
 	} else if (employee.wardid == 4){
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
 				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
 
-		$('#actionList').append('<li><a href="tel:' + managerPhone + '"><h3>Call Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 
-		$('#actionList').append('<li><a href="mailto:' + managerEmail + '"><h3>Email Bank Manager</h3>' +
-				'<p>' + managerEmail + '</p></a></li>');
-
-		$('#actionList').append('<li><a href="sms:' + managerPhone + '"><h3>SMS Bank Manager</h3>' +
-				'<p>' + managerPhone + '</p></a></li>');
 	}
 	
 	$('#actionList').listview('refresh');
