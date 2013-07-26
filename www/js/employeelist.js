29e36d67edae7e6062f06aebb4cae13c8ca58bf7
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-26 15:46:33 +0100

    change employeelist.js

--- www/js/employeelist.js
+++ www/js/employeelist.js
@@ -14,9 +14,9 @@ function getEmployeeList() {
 		$.each(employees, function(index, employee) {
 			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
 					'<img src="pics/' + employee.picture + '"/>' +
-					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<h4>' + employee.fname + ' ' + employee.lname + '</h4>' +
 					'<p>' + employee.title + '</p>' +
-					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+					'</a></li>');
 		});
 		$('#employeeList').listview('refresh');
 	});
