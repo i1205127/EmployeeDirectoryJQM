e7636d9fb034c163cf2b9754346a1a0043663a64
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-26 18:02:22 +0100

    add wardsisterlist.js

---
+++ www/js/wardsisterlist.js
@@ -0,0 +1,23 @@
+//var serviceURL = "http://localhost/directory/services/";
+var serviceURL = "http://polgahawelatown.com/EmployeeDirectoryJQM/services/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getwardsisters.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="pics/' + employee.picture + '"/>' +
+					'<h4>' + employee.fname + ' ' + employee.lname + '</h4>' +
+					'<p>' + employee.wardid + '</p>' +
+					'</a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
