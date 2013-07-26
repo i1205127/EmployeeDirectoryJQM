e3701a8c1d275e73366a7903e7e1e28434bf46ed
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-26 21:39:26 +0100

    ate

--- www/js/wardsisterdetails.js
+++ www/js/wardsisterdetails.js
@@ -11,7 +11,7 @@ function displayEmployee(data) {
 	$('#employeeTitle').text(employee.title);
 	$('#city').text(employee.name);
 	console.log(employee.officePhone);
-	if (employee.name == "Emergency") {
+	if (employee.name == "Operation") {
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.name + '"><h3>View Manager</h3>' +
 				'<p>' + employee.id + ' ' + employee.name + '</p></a></li>');
 	}
