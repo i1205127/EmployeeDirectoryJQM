96d6af6705cfbd260cc992995c163f825d66e845
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-26 22:45:27 +0100

    sdfa

--- www/js/wardsisterdetails.js
+++ www/js/wardsisterdetails.js
@@ -11,29 +11,40 @@ function displayEmployee(data) {
 	$('#employeeTitle').text(employee.title);
 	$('#city').text(employee.wardname);
 	console.log(employee.officePhone);
-	if (employee.wardname == "Emergency") {
+	if (employee.wardid == 1) {
 		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
-				'<p>' + employee.id + ' ' + employee.wardname + '</p></a></li>');
+				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
+	} else if (employee.wardid == 2){
+		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.wardid + '"><h3>View Shifts</h3>' +
+				'<p>' + employee.wsfname + ' ' + employee.wardname + '</p></a></li>');
 	}
-	if (employee.reportCount>0) {
-		$('#actionList').append('<li><a href="reportlist.html?id=' + employee.id + '"><h3>View Direct Reports</h3>' +
-				'<p>' + employee.reportCount + '</p></a></li>');
-	}
-	if (employee.email) {
-		$('#actionList').append('<li><a href="mailto:' + employee.email + '"><h3>Email</h3>' +
-				'<p>' + employee.email + '</p></a></li>');
-	}
-	if (employee.officePhone) {
-		$('#actionList').append('<li><a href="tel:' + employee.officePhone + '"><h3>Call Office</h3>' +
-				'<p>' + employee.officePhone + '</p></a></li>');
-	}
-	if (employee.cellPhone) {
-		$('#actionList').append('<li><a href="tel:' + employee.cellPhone + '"><h3>Call Cell</h3>' +
-				'<p>' + employee.cellPhone + '</p></a></li>');
-		$('#actionList').append('<li><a href="sms:' + employee.cellPhone + '"><h3>SMS</h3>' +
-				'<p>' + employee.cellPhone + '</p></a></li>');
-	}
-	$('#actionList').listview('refresh');
+	
+	
+	
+	
+	
+	
+	
+	
+//	if (employee.reportCount>0) {
+//		$('#actionList').append('<li><a href="reportlist.html?id=' + employee.id + '"><h3>View Direct Reports</h3>' +
+//				'<p>' + employee.reportCount + '</p></a></li>');
+//	}
+//	if (employee.email) {
+//		$('#actionList').append('<li><a href="mailto:' + employee.email + '"><h3>Email</h3>' +
+//				'<p>' + employee.email + '</p></a></li>');
+//	}
+//	if (employee.officePhone) {
+//		$('#actionList').append('<li><a href="tel:' + employee.officePhone + '"><h3>Call Office</h3>' +
+//				'<p>' + employee.officePhone + '</p></a></li>');
+//	}
+//	if (employee.cellPhone) {
+//		$('#actionList').append('<li><a href="tel:' + employee.cellPhone + '"><h3>Call Cell</h3>' +
+//				'<p>' + employee.cellPhone + '</p></a></li>');
+//		$('#actionList').append('<li><a href="sms:' + employee.cellPhone + '"><h3>SMS</h3>' +
+//				'<p>' + employee.cellPhone + '</p></a></li>');
+//	}
+//	$('#actionList').listview('refresh');
 	
 }
 
