f547559ad4c8f833a4a6f3b94e823dae5e5f7c53
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 17:09:22 +0100

    asdf

--- www/js/reportlist.js
+++ www/js/reportlist.js
@@ -2,13 +2,7 @@ $('#reportListPage').live('pageshow', function(event) {
 	var id = getUrlVars()["id"];
 	console.log("reports for " + id);
 	$.getJSON(serviceURL + 'getreports.php?id='+id, function (data) {
-		var reports = data.items;
-		$('#reportList').append('<li>' +
-					'<p>' + "teeteas" + '</p>' +
-					'</li>');
-		
-		
-		$.each(reports, function(index, employee) {
+				$.each(reports, function(index, employee) {
 			$('#reportList').append('<li><a href="shiftlist.html?id=' + employee.sid + '">' +
 					'<h4>' + employee.stime + ' ' + employee.sid + '</h4>' +
 					'<p>' + employee.sdate + '</p>' +
--- www/js/shiftlist.js
+++ www/js/shiftlist.js
@@ -1,21 +1,14 @@
-var shiftdata;
-
-$('#shiftListPage').live('pageshow', function(event) {
+$('#reportListPage').live('pageshow', function(event) {
 	var id = getUrlVars()["id"];
 	console.log("reports for " + id);
-	$.getJSON(serviceURL + 'getshift.php?id='+id, getShifts);
-});
-
-function getShifts(data){
-		
-		shiftdata = data.item;
-		
-		$.each(shiftdata, function(index, employee) {
-			$('#shiftList').append('<li><a href="shiftlist.html?id=' + employee.sid + '">' +
-					'<h4>' + employee.stime + ' ' + employee.sid + '</h4>' +
-					'<p>' + employee.sdate + '</p>' +
+	$.getJSON(serviceURL + 'getreports.php?id='+id, function (data) {
+		var reports = data.items;
+		$.each(reports, function(index, employee) {
+			$('#reportList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
 					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
 		});
-		$('#shiftList').listview('refresh');
+		$('#reportList').listview('refresh');
 	});
 });
