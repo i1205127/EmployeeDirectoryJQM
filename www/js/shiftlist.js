7f2f5462f3a09a14fffd948ca0199f2d2177ef44
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 11:21:32 +0100

    asdf

--- www/js/shiftlist.js
+++ www/js/shiftlist.js
@@ -1,7 +1,7 @@
 //var serviceURL = "http://localhost/directory/services/";
 var serviceURL = "http://polgahawelatown.com/EmployeeDirectoryJQM/services/";
 
-var shift;
+var shift : [];
 
 $('#shiftdetailsPage').bind('pageinit', function(event) {
 	getShiftList();
@@ -11,6 +11,7 @@ function getShiftList() {
 	$.getJSON(serviceURL + 'getshifts.php', function(data) {
 		$('#employeeList li').remove();
 		shift = data.items;
+		
 		$.each(shift, function(index, shift) {
 			$('#shiftList').append('<li>' +
 					'<h4>' + shift.sdate + ' ' + shift.stime + '</h4>' +
