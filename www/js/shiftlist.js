eb1876645b2eeabf75d21f46e9fb07db786a0e6a
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 18:33:57 +0100

    asdfasdf

--- www/js/shiftlist.js
+++ www/js/shiftlist.js
@@ -2,7 +2,7 @@ $('#shiftListPage').live('pageshow', function(event) {
 	var id = getUrlVars()["id"];
 	console.log("reports for " + id);
 	$.getJSON(serviceURL + 'getshift.php?id='+id, function (data) {
-		var reports = data.items;
+		var reports = data.item;
 		$.each(reports, function(index, employee) {
 			$('#shiftList').append('<li><a href="employeedetails.html?id=' + employee.sid + '">' +
 					'<h4>' + employee.sdate + ' ' + employee.stime + '</h4>' +
