1a50e115c756fa08ffef362073a19622caf8a789
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 21:47:08 +0100

    remove

--- www/js/shiftlist.js
+++ www/js/shiftlist.js
@@ -4,11 +4,8 @@ $('#shiftListPage').live('pageshow', function(event) {
 	$.getJSON(serviceURL + 'getshift.php?id='+id, function (data) {
 		var reports = data.item;
 		$.each(reports, function(index, employee) {
-			$('#reportList').append('<li><a href="employeedetails.html?id=' + employee.sid + '">' +
-					'<h4>' + employee.sdate + ' ' + employee.stime + '</h4>' +
-					'<p>' + employee.sfilled + '</p>' +
-					'<span class="ui-li-count">' + employee.sfilled + '</span></a></li>');
-
+			$('#shiftList').append('<h4>' + employee.sdate + ' ' + employee.stime + '</h4>' +
+					'<span class="ui-li-count">' + employee.sfilled + '</span></a>');
 		});
 		$('#shiftList').listview('refresh');
 	});
