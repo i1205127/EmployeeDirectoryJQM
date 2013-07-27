6f81b2dcefbdc964981ef6701fab4070c64290d3
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 16:11:19 +0100

    adfadfa

--- www/js/reportlist.js
+++ www/js/reportlist.js
@@ -3,6 +3,11 @@ $('#reportListPage').live('pageshow', function(event) {
 	console.log("reports for " + id);
 	$.getJSON(serviceURL + 'getreports.php?id='+id, function (data) {
 		var reports = data.items;
+		$('#reportList').append('<li>' +
+					'<p>' + "teeteas" + '</p>' +
+					'</li>');
+		
+		
 		$.each(reports, function(index, employee) {
 			$('#reportList').append('<li><a href="shiftlist.html?id=' + employee.sid + '">' +
 					'<h4>' + employee.stime + ' ' + employee.sid + '</h4>' +
