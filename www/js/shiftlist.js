a3395e8156f6fa7a9587266aa28971057e877582
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 16:21:50 +0100

    asdf

--- www/js/shiftlist.js
+++ www/js/shiftlist.js
@@ -1,29 +1,23 @@
+//var serviceURL = "http://localhost/directory/services/";
+var serviceURL = "http://polgahawelatown.com/EmployeeDirectoryJQM/services/";
 
-var reports;
+var employees;
 
-$('#shiftListPage').live('pageshow', function(event) {
-	var id = getUrlVars()["id"];
-	console.log("reports for " + id);
-	$.getJSON(serviceURL + 'getshift.php?id='+id, function (data) {
-		var reports = data.items;
-		$.each(reports, function(index, reports) {
-			$('#shiftList').append('<li><a href="employeedetails.html?id=' + reports.sid + '">' +
-					'<h4>' + reports.sdate + ' ' + reports.stime + '</h4>' +
-					'<p>' + reports.sfiled + '</p>' +
-					'<span class="ui-li-count">' + reports.reportCount + '</span></a></li>');
+$('#shiftListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getwardsisters.php', function(data) {
+		$('#shiftList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#shiftList').append('<li><a href="wardsisterdetails.html?id=' + employee.wsid + '">' +
+					'<img src="pics/' + employee.wspicture + '"/>' +
+					'<h4>' + employee.wsfname + ' ' + employee.wslname + '</h4>' +
+					'<p>' + employee.wardid + '</p>' +
+					'</a></li>');
 		});
 		$('#shiftList').listview('refresh');
 	});
-});
-
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
 }
