cda33dc111bcd19346cc9bc3e51fb8500f88d04d
Amith Ranasinghage <i1205127@my.shu.ac.uk>
2013-07-27 15:44:36 +0100

    asd

--- www/js/wardsisterlist.js
+++ www/js/wardsisterlist.js
@@ -12,7 +12,7 @@ function getEmployeeList() {
 		$('#wardSisterList li').remove();
 		employees = data.items;
 		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.wsid + '">' +
+			$('#wardSisterList').append('<li><a href="employeedetails.html?id=' + employee.wsid + '">' +
 					'<img src="pics/' + employee.wspicture + '"/>' +
 					'<h4>' + employee.wsfname + ' ' + employee.wslname + '</h4>' +
 					'<p>' + employee.wardid + '</p>' +
